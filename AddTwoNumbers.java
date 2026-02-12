0f2f6c7467fa505f01e927d44ebd6e7da51e8ef5
mikey0925 <mohitchauhan0925@gmail.com>
2026-02-12 23:53:06 +0530

    add two number

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 5;
+        int num2 = 10;
+
+        // Add the two numbers and store the result in a new variable
+        int sum = num1 + num2;
+
+        // Print the result to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
